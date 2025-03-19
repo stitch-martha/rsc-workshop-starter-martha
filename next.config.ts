9704745d9c1b18266499d573f3ab6343ce48e356
Martha Bowler <martha@stitchhealth.co>
2025-03-19 14:56:48 +0000

    Add stale revalidate

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    // This is experimental but it stops the stale-while-revalidate from being too aggressive
+    staleTimes: {
+      dynamic: 30
+    },
+  },
   images: {
     remotePatterns: [
       {
