fd29eeec783281255ccaba7d4f05ac6d255e830b
Martha Bowler <martha@stitchhealth.co>
2025-03-19 15:33:08 +0000

    Add mutate data server function

--- app/(intro)/client-server/_components/ClientComponent.tsx
+++ app/(intro)/client-server/_components/ClientComponent.tsx
@@ -5,17 +5,20 @@ import React from 'react'
 type Props  ={
   children?: React.ReactNode
   content?: React.ReactNode
-
+  mutateData: () => Promise<string >
 }
 
 
-export default function ClientComponent({children, content }:Props) {
+export default function ClientComponent({children, content, mutateData }:Props) {
   // Without use client, we cannot use onClick, useState, use Effect, window.location, custom hooks
 
   return (
     <div className='border-2 border-blue-500 p-4'>
       {content}
-    <button onClick={()=> {return alert('Hello from the client')}}>Hello</button>
+    <button onClick={async()=> {
+      const data = await mutateData()
+      return alert(data)}
+      }>Hello</button>
       {children}
     </div>
   )
--- app/(intro)/client-server/page.tsx
+++ app/(intro)/client-server/page.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import { prisma } from '@/db'
 import ClientComponent from './_components/ClientComponent'
 import ServerComponent from './_components/ServerComponent'
 
 export default function ClientServerPage() {
   // We can wrap our client components around server components without converting them
+
+  async function mutateData() {
+    // The function will have a hidden API endpoint created for it and can be called from client
+    // React 19 directive - this is a reference to an endpoint
+    'use server'
+    const data = await prisma.contact.findMany()
+    return data[0].first || ''
+  }
+
   return (
     <div>
       ClientServerPage
-      <ClientComponent content={<ServerComponent />}>
+      <ClientComponent mutateData={mutateData} content={<ServerComponent />}>
         <ServerComponent />
       </ClientComponent>
     </div>
